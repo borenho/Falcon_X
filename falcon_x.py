443c9dc420341ae6ca0730bf74a8badbfe177d3d
Mugwe <mugwetracy@gmail.com>
2017-06-16 15:39:40 +0300

    Add function to update price

--- falcon_x.py
+++ falcon_x.py
@@ -1,6 +1,10 @@
 total_seats = 0
-
+price = 1000
 
 # Method to let staff create new seats
 def create_seats(number_of_seats):
     total_seats = number_of_seats
+
+
+def update_price(new_price):
+    price = new_price
