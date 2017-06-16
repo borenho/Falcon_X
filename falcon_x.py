1e4a8d965c187343e8bd76977a409f3137b38fb4
Mugwe <mugwetracy@gmail.com>
2017-06-16 16:34:11 +0300

    Update passenger manifests

--- falcon_x.py
+++ falcon_x.py
@@ -2,6 +2,7 @@ from random import randint
 
 total_seats = 0
 price = 1000
+    
 passenger_list = []
 passenger_dict = {}
 
@@ -18,11 +19,12 @@ def update_price(new_price):
 
 def book_seat(name):
     ticket_id = randint(1, total_seats)
-    
     passenger_dict.update({"Ticket ID": ticket_id, "Name": name})
+    
     passenger_list.append(passenger_dict)
 
 
 def passenger_manifests(passenger_list):
-    print (passenger_list)
+    for i in passenger_list:
+        print(i["Ticket ID"]+ ':' + i["Name"])
  
